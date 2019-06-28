f9f72febf2a7a112a6d47a00c9634e515d1ed0a8
thomas brown <thomasbrown9101@gmail.com>
2019-06-27 18:31:08 -0700

    completed react framework (need to add css)

---
+++ react-3-afternoon/src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import data from "./data";
+import './App.css';
+import Card from "./components/card";
+import NavBar from './components/navbar';
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data:data,
+      index: 0
+    }
+
+    this.goForward = this.goForward.bind(this)
+    this.goBackward = this.goBackward.bind(this)
+  }
+
+
+  goForward(){
+    let { index, data,} = this.state
+    if(data[index + 1]){
+      this.setState({
+        index:this.state.index += 1
+      })
+    }
+    else{
+      console.log("youve reached the end of the array")
+      this.setState({
+        index:0
+      })
+    }
+
+    
+  }
+
+  goBackward(){
+    let {index, data} = this.state
+    if(data[index - 1]){
+      this.setState({
+        index:this.state.index -= 1
+      })
+    }
+    else{
+      console.log("youve reached the beginning")
+      this.setState({
+        index:24
+      })
+    }
+  
+    
+  }
+
+
+
+  render(){
+    return (
+      
+      <div className="App">
+         hi
+         <Card parentData = {this.state.data[this.state.index]} />
+         <NavBar goForwardFn = {this.goForward}
+                 goBackwardFn = {this.goBackward}/>
+      </div>
+    )
+  }
+  
+}
+
+export default App;
